fbe54730ba939d0e30604b62bdb2d9ce97d72878
Shakena <shakenacme@gmail.com>
2021-08-20 23:07:04 -0400

    Added to js hamburger click event to add/remove fade-in/out animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,19 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const mobileNav = document.querySelector('.header__mobileNav');
 
+
 // Mobile Navigation toggle
 btnHamburger.addEventListener('click', function() {
    if (btnHamburger.classList.contains('open')) {
       btnHamburger.classList.remove('open');
       mobileNav.classList.remove('open');
+      mobileNav.classList.remove('fade-in');
+      mobileNav.classList.add('fade-out');
    }
    else {
       btnHamburger.classList.add('open');
       mobileNav.classList.add('open');
+      mobileNav.classList.remove('fade-out');
+      mobileNav.classList.add('fade-in');
    }
 })
