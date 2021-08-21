9b925f12a5027c0cdd176cf61a9b7f1c321f422b
Shakena <shakenacme@gmail.com>
2021-08-21 00:06:07 -0400

    Added overflow visibile/hidden to body when mobilenav is open/closed to stop scrolling

--- app/js/script.js
+++ app/js/script.js
@@ -8,16 +8,17 @@ const bodyTag = document.querySelector("body");
 btnHamburger.addEventListener('click', function() {
    if (header.classList.contains('open')) {
 
-      //added open class to header (the parent element) and removed from btnhamburger
-      // remove open -- close mobile nav
+      // close mobile nav
       header.classList.remove('open');
       mobileNav.classList.remove('fade-in');
       mobileNav.classList.add('fade-out');
+      bodyTag.style.overflow = "visible";
    }
    else {
-      // add open -- open mobile nav
+      // open mobile nav
       header.classList.add('open');
       mobileNav.classList.remove('fade-out');
       mobileNav.classList.add('fade-in');
+      bodyTag.style.overflow = "hidden";
    }
 })
