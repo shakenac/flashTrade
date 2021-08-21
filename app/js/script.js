2d4550bf8369368eb406c1e59ea14bd568cb5bbf
Shakena <shakenacme@gmail.com>
2021-08-20 22:56:51 -0400

    Created js function to show/hide mobile nav when hamburger is clicked

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,14 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const mobileNav = document.querySelector('.header__mobileNav');
 
-// If hamburger is clicked, add open class
+// Mobile Navigation toggle
 btnHamburger.addEventListener('click', function() {
    if (btnHamburger.classList.contains('open')) {
       btnHamburger.classList.remove('open');
+      mobileNav.classList.remove('open');
    }
    else {
       btnHamburger.classList.add('open');
+      mobileNav.classList.add('open');
    }
-
 })
